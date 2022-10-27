182fcc2dadfea7468882edee19cd0f8b86b63dbf
Darren Kulback <dkulback@gmail.com>
2022-10-27 09:36:19 -0600

    Add before action when location param is blank

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  def check_location
+    if params[:location].nil? || params[:location].blank?
+      render json: { error: 'Location parameter is required' },
+             status: :bad_request
+    end
+  end
 end
